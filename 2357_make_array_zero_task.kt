06c0e1f48f26c2ec72b0f719012a2f5ec7943145
unknown <egor.nikonenko29@gmail.com>
2024-07-20 13:03:21 +0300

    use replaceAll

--- 2357_make_array_zero_task.kt
+++ 2357_make_array_zero_task.kt
@@ -1,18 +1,14 @@
 class Solution {
-    fun minimumOperations(nums: IntArray): Int {
+    fun minimumOperations(input_nums: IntArray): Int {
         var num_operation = 0
+        var nums = input_nums.toMutableList()
         while (true) {
             val filtred_nums = nums.filter { it > 0 }
             if (filtred_nums.isEmpty()) {
                 break
             }
             val min_element = filtred_nums.min()
-            for (index in nums.indices)/*
-        Когда я пишу for(number in nums), number считается val
-        и я не могу её менять(а очень хочется)
-        */ {
-                nums[index] -= min_element
-            }
+            nums.replaceAll { it - min_element }
             num_operation++
         }
         return num_operation
